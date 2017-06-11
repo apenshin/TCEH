ab9671580b1580a468c41b932ff9ba3d89d4a186
Boris Rubin <borisrubin@yahoo.com>
2017-06-11 20:22:30 +0300

    tmp.py changed to empty

--- tmp.py
+++ tmp.py
@@ -1,15 +1,7 @@
-import random
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
+print('F от 5 =', factorial(997))
 
-def raise_random_error():
-    ex = random.choice([ValueError, TypeError, RuntimeError])
-    raise ex('Message for a user')
-
-try:
-    raise_random_error()
-except ValueError as e:
-    print('Value', e)
-except TypeError as e:
-    print('Type', e)
-except RuntimeError as e:
-    print('Runtime', e)
