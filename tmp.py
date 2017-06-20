00fb7fb02745cc1be752216c39ed3817798c7320
Boris Rubin <borisrubin@yahoo.com>
2017-06-20 11:05:25 +0300

    tmp.py с началом дз5

--- tmp.py
+++ tmp.py
@@ -5,9 +5,32 @@
 #    который позволяет добавить другого человека в список знакомых.
 # И метод is_known(person), который возвращает знакомы ли два человека
 
-a = ('s','d','s','e','f')
+class Person(object):
 
-print(a.__str__())
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+        self.known_persons = []
+
+    def know(self, person):
+        if not person in self.known_persons:
+            self.known_persons.append(person)
+        else:
+            print('{} уже знает человека по имени {}'.format(self.name, person.name))
+
+    def is_known(self, person):
+        text_action = {True : 'знает человека по имени', False : 'не знает человека по имени'}
+        is_known_person = person in self.known_persons
+        print('{} {} {}'.format(self.name, text_action[is_known_person], person.name))
+
+
+petr = Person('Петр', 30)
+olga = Person('Ольга', 26)
+
+petr.is_known(olga)
+petr.know(olga)
+petr.is_known(olga)
+petr.know(olga)
 
 
 
